5bc212d9443a7e98ed00764977369c7e5e6383a1
tobygameac <tobyreallygameac@gmail.com>
2015-12-21 19:16:57 +0800

    scene marker visible detector

---
+++ Unity Projects/AR TD/Assets/TD/new/Scripts/AR/GameSceneFrameMarkerVisibleDetector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class GameSceneFrameMarkerVisibleDetector : MonoBehaviour {
+
+  public GameObject coreFrameMarkerMissingCanvas;
+
+  private float originalTimeScale;
+
+  private MeshRenderer meshRenderer;
+
+  private static Game game;
+
+  void Start() {
+    meshRenderer = GetComponent<MeshRenderer>();
+
+    if (game == null) {
+      game = Camera.main.GetComponent<Game>();
+    }
+  }
+
+  void Update() {
+    if (game.SystemState == GameConstants.SystemState.PLAYING && (game.gameState == GameConstants.GameState.MIDDLE_OF_THE_WAVE || game.gameState == GameConstants.GameState.WAIT_FOR_THE_NEXT_WAVE)) {
+      if (meshRenderer.enabled) {
+        coreFrameMarkerMissingCanvas.SetActive(false);
+        Time.timeScale = 1.0f;
+      } else {
+        coreFrameMarkerMissingCanvas.SetActive(true);
+        Time.timeScale = 0;
+      }
+    } else {
+      coreFrameMarkerMissingCanvas.SetActive(false);
+      Time.timeScale = 0;
+    }
+  }
+}
