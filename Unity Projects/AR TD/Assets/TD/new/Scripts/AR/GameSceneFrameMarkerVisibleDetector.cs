9e5f12e22cb23cb089d67717e0c23acb80d3dd8d
tobygameac <tobyreallygameac@gmail.com>
2016-01-13 17:36:45 +0800

    fix marker missing detector

--- Unity Projects/AR TD/Assets/TD/new/Scripts/AR/GameSceneFrameMarkerVisibleDetector.cs
+++ Unity Projects/AR TD/Assets/TD/new/Scripts/AR/GameSceneFrameMarkerVisibleDetector.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using Vuforia;
 using System.Collections;
 
-[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MarkerBehaviour))]
 public class GameSceneFrameMarkerVisibleDetector : MonoBehaviour {
 
   public GameObject coreFrameMarkerMissingCanvas;
@@ -9,25 +10,28 @@ public class GameSceneFrameMarkerVisibleDetector : MonoBehaviour {
   private float originalTimeScale;
 
   private MeshRenderer meshRenderer;
+  private MarkerBehaviour markerBehaviour;
 
   private static Game game;
 
   void Start() {
-    meshRenderer = GetComponent<MeshRenderer>();
-
     if (game == null) {
       game = Camera.main.GetComponent<Game>();
     }
+
+    markerBehaviour = GetComponent<MarkerBehaviour>();
   }
 
   void Update() {
     if (game.SystemState == GameConstants.SystemState.PLAYING && (game.gameState == GameConstants.GameState.MIDDLE_OF_THE_WAVE || game.gameState == GameConstants.GameState.WAIT_FOR_THE_NEXT_WAVE)) {
-      if (meshRenderer.enabled) {
-        coreFrameMarkerMissingCanvas.SetActive(false);
-        Time.timeScale = 1.0f;
-      } else {
+      if (markerBehaviour.CurrentStatus == TrackableBehaviour.Status.NOT_FOUND ||
+        markerBehaviour.CurrentStatus == TrackableBehaviour.Status.UNDEFINED ||
+        markerBehaviour.CurrentStatus == TrackableBehaviour.Status.UNKNOWN) {
         coreFrameMarkerMissingCanvas.SetActive(true);
         Time.timeScale = 0;
+      } else {
+        coreFrameMarkerMissingCanvas.SetActive(false);
+        Time.timeScale = 1.0f;
       }
     } else {
       coreFrameMarkerMissingCanvas.SetActive(false);
