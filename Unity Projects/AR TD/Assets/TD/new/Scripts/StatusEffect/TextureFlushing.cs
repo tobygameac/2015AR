3cf3b82ccd18efeb8b22cdde1eaa4a31e8a1a43f
ben_LAB <bigben59456@gmail.com>
2016-01-17 20:09:44 +0800

    arrow with color floating

---
+++ Unity Projects/AR TD/Assets/TD/new/Scripts/StatusEffect/TextureFlushing.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextureFlushing : MonoBehaviour
+{
+    public GameObject icon;
+    private SpriteRenderer iconImg;
+
+    public Sprite[] sprites = new Sprite[5];
+
+    public float floatingMagnitude;
+    public float floatingSpeed;
+
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+        iconImg = icon.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float input_angle = -Mathf.PI / 2 + (floatingSpeed * Time.time) % Mathf.PI;
+
+        Vector3 newPosition = originalPosition + transform.forward * floatingMagnitude * Mathf.Sin(input_angle);
+        transform.localPosition = newPosition;
+
+        input_angle += Mathf.PI / 2;
+
+        if (input_angle < Mathf.PI / 5) iconImg.sprite = sprites[4];
+        else if (input_angle < (Mathf.PI / 5) * 2) iconImg.sprite = sprites[3];
+        else if (input_angle < (Mathf.PI / 5) * 3) iconImg.sprite = sprites[2];
+        else if (input_angle < (Mathf.PI / 5) * 4) iconImg.sprite = sprites[1];
+        else iconImg.sprite = sprites[0];
+
+    }
+
+    void Disable()
+    {
+        transform.localPosition = originalPosition;
+    }
+}
