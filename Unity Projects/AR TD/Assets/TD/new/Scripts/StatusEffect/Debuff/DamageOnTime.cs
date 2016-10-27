7995c49de27d50b8f9cb901aa5646f44c1fb3727
tobygameac <tobyreallygameac@gmail.com>
2016-10-27 15:04:11 +0800

    ignore some files

---
+++ Unity Projects/AR TD/Assets/TD/new/Scripts/StatusEffect/Debuff/DamageOnTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterStats))]
+public class DamageOnTime : MonoBehaviour {
+
+  private CharacterStats characterStats;
+
+  public float damage;
+
+  void Start() {
+    characterStats = GetComponent<CharacterStats>();
+  }
+
+  void Update() {
+    characterStats.CurrentHP -= damage * Time.deltaTime;
+  }
+
+}
